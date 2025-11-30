f5cdaddea261180644b5861a1443ceeb36793396
Jacob Homanics <32080359+JacobHomanics@users.noreply.github.com>
2025-11-30 16:50:18 -0600

    heal spell

--- Assets/Scripts/TargetManagerAttackerSpin.cs
+++ Assets/Scripts/TargetManagerAttackerSpin.cs
@@ -4,6 +4,7 @@ using JacobHomanics.Core.OverlapShape;
 using JacobHomanics.HealthSystem;
 using UnityEngine;
 using UnityEngine.Events;
+using JacobHomanics.Timer.Extensions;
 
 public class TargetManagerSpin : MonoBehaviour
 {
@@ -27,6 +28,86 @@ public class TargetManagerSpin : MonoBehaviour
 
     public UnityEvent OnSuccess;
 
+    public TimedAction castbar;
+
+    [System.Serializable]
+    public struct Spell
+    {
+        public Sprite sprite;
+        public string name;
+        public float castTime;
+
+        public string action;
+    }
+
+    public Spell spell1;
+
+
+    public Spell CastingSpell
+    {
+        get;
+        private set;
+    }
+
+    void OnEnable()
+    {
+        if (castbar)
+            castbar.OnTimeComplete.AddListener(OnDurationReached);
+    }
+
+    void OnDisable()
+    {
+        if (castbar)
+            castbar.OnTimeComplete.RemoveListener(OnDurationReached);
+
+    }
+
+    public void Cast(Spell spell)
+    {
+        castbar.Cast(spell.sprite, spell.name, spell.castTime);
+        CastingSpell = spell;
+    }
+
+
+    void OnDurationReached()
+    {
+        DoSpell();
+    }
+
+
+    void DoSpell()
+    {
+        if (selfHealth)
+        {
+            var diff = damage * (variancePercentage / 100f);
+            var rn = Random.Range(damage - diff, damage + diff);
+            selfHealth.Heal(rn);
+            OnSuccess?.Invoke();
+            return;
+        }
+    }
+
+
+    void Update()
+    {
+        if (!selfHealth)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            castbar.CancelCast();
+        }
+
+        if (Input.GetKey(KeyCode.E) || Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
+            castbar.CancelCast();
+
+    }
+
+    public void StartCast()
+    {
+        Cast(spell1);
+    }
+
     public void TryAttack()
     {
 
@@ -37,14 +118,6 @@ public class TargetManagerSpin : MonoBehaviour
             return;
         }
 
-        if (selfHealth)
-        {
-            var diff = damage * (variancePercentage / 100f);
-            var rn = Random.Range(damage - diff, damage + diff);
-            selfHealth.Heal(rn);
-            OnSuccess?.Invoke();
-            return;
-        }
 
         List<Transform> hitRoots = new List<Transform>();
 
